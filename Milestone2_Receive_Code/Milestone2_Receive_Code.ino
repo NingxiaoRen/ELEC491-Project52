5bb980d7ce971503586258da0b20831df8c077ca
Ningxiao Ren <simonren1993@gmail.com>
2019-01-20 22:54:17 -0800

    minor changes; add comments

--- Milestone2_Receive_Code/Milestone2_Receive_Code.ino
+++ Milestone2_Receive_Code/Milestone2_Receive_Code.ino
@@ -6,10 +6,17 @@
 #define CD_PD 19
 
 #define SIZE 200
-
+  /******************************************************************************
+     CD_PD   REG_DATA   RXTX                       MODE
+                HIGH    HIGH     Control regiter read through MOSI - RXD/OUTPUT
+                HIGH     LOW     Control regiter write through MISO - TXD/INPUT
+                LOW     HIGH          Data read through MOSI - RXD/OUTPUT
+		        LOW      LOW          Data write through MISO - TXD/INPUT
+   CD_PD High: Not transfer;  LOW: Transfer
+  *******************************************************************************/
 void SPI_SlaveInit(void);
 byte SPI_SlaveReceive(void);
-void modem_correction (uint8_t);
+void DataCorrection_Transmit (uint8_t);
 
 volatile byte buf[SIZE], check[3];
 volatile uint16_t pos_write = 0;
@@ -22,69 +29,16 @@ void setup() {
   Serial.begin(115200);
   SPI_SlaveInit();
   
-  //configure modem
-  pinMode(RXTX, OUTPUT);
-  pinMode(REG_DATA, OUTPUT);
-  pinMode(CD_PD, INPUT);
-  
   // now turn on interrupts
-  SPI.attachInterrupt();
+  // SPI.attachInterrupt();
   digitalWrite(RXTX, HIGH);
   delay(100);
-  
   digitalWrite(LED, LOW);
 }
 
-// SPI interrupt routine
-ISR (SPI_STC_vect){
-  
-//if (CD_PD == LOW){
-  buf[pos_write] = SPDR;
-  pos_write++;
-  if(pos_write>SIZE-1) pos_write = 0;
-//}
-  if (buf[pos_write] != 0){
-    if (buf[pos_write - 1] == 0){
-      Serial.println(0);
-    }
-    Serial.println(buf[pos_write]);
-  }
-
-}  // end of interrupt routine SPI_STC_vect
-
-//To stop the interrupt we need to set SS Low, which means that there is nothing in the line
-//To continue we need to set the SS high
-
-ISR (PCINT1_vect){
-  if(CD_PD == LOW){
-    PORTB &= ~(1<<PB1);
-  }else{
-    PORTB |= (1<<PB1);
-  }
-}
- 
 void loop() {
 
-  // The following commented code 1`reads from the control registers
-  /*digitalWrite(REG_DATA, HIGH);
-  digitalWrite(RXTX, HIGH);
-  byte temp1,temp2,temp3;
-  //while ((PINB  &   (1<<SCK))!=0);
-  temp1 = SPI_SlaveReceive();
-  temp2 = SPI_SlaveReceive();
-  temp3 = SPI_SlaveReceive();
-//delay(10);
-  Serial.println(temp1,BIN);
-  Serial.println(temp2,BIN);
-  Serial.println(temp3,BIN);*/  
-
-//  if(buf[pos_write] == 170){
-//    if (buf[pos_write - 1] == 170){
-//      if (buf[pos_write - 2] == 170){
-//        digitalWrite(LED, HIGH);
-//      }
-//    }
-//  }
+//Modem_CtrlRead()
 
 //From the transmission, we know that we send a header byte which corresponds to the device
 
@@ -120,17 +74,70 @@ if (pos_read > SIZE - 1) pos_read = 0;
 
 }
 
-void SPI_SlaveInit(void){
-  pinMode(SCK,INPUT);
-  pinMode(MOSI,INPUT);
-  pinMode(SS,INPUT);
-  pinMode(MISO,OUTPUT);
-  //Enable SPI
-  SPCR = (1<<SPE)|(0<<MSTR);
-  
-  //SPSR =(1<<SPI2X);
+/*******************************************************************************
+* Function Name  : ISR
+* Description    : Interrupt Service Routine for SPI transfer complete
+* Input          : SPI transfer complete
+* Output         : Print data in SPI buffer
+*******************************************************************************/
+ISR (SPI_STC_vect){
+//if (CD_PD == LOW){
+  buf[pos_write] = SPDR;
+  pos_write++;
+  if(pos_write>SIZE-1) pos_write = 0;
+//}
+  if (buf[pos_write] != 0){
+    if (buf[pos_write - 1] == 0){
+      Serial.println(0);
+    }
+    Serial.println(buf[pos_write]);
+  }
+} 
+
+/*******************************************************************************
+* Function Name  : ISR
+* Description    : Interrupt Service Routine for PCINT1/PB1/D9 changes (SS/PCINT2/PB2)
+* Input          : SPI transfer complete
+* Output         : Print data in SPI buffer
+*******************************************************************************/
+ISR (PCINT1_vect){
+  //To stop the interrupt we need to set SS ?? PB1 ??  Low, which means that there is nothing in the line
+  //To continue we need to set the SS high
+  if(CD_PD == LOW){
+    PORTB &= ~(1<<PB1);
+  }else{
+    PORTB |= (1<<PB1);
+  }
 }
 
+/*******************************************************************************
+* Function Name  : SPI_SlaveInit
+* Description    : Configure Arduino pins for slave mode
+* Input          : None
+* Output         : None
+* Return         : SPDR
+*******************************************************************************/
+void SPI_SlaveInit(void){
+  /* Configure Arduino as a slave, Set MISO output, all other input */
+  pinMode(SCK,INPUT);   // PB5 --- GND
+  pinMode(SS,INPUT);    // PB2 --- ST7540_CLR/T
+  pinMode(MOSI,INPUT);  // PB3 --- ST7540_RXD/OUTPUT
+  pinMode(MISO,OUTPUT); // PB4 --- ST7540_TXD/INPUT
+  /* Configure pins for ST7540 */
+  pinMode(RXTX, OUTPUT);
+  pinMode(REG_DATA, OUTPUT);
+  pinMode(CD_PD, INPUT);
+  /* Enable SPI */
+  SPCR = (1<<SPE)|(0<<MSTR);
+}
+
+/*******************************************************************************
+* Function Name  : SPI_SlaveReceive
+* Description    : Arduino SPI receives data from the buffer ( Modem )
+* Input          : None
+* Output         : None
+* Return         : SPDR
+*******************************************************************************/
 byte SPI_SlaveReceive(void){
   /* Wait for reception complete */
   while(!(SPSR & (1<<SPIF)));
@@ -138,33 +145,82 @@ byte SPI_SlaveReceive(void){
   return SPDR;
 }
 
+/*******************************************************************************
+* Function Name  : SPI_SlaveTransmit
+* Description    : Arduino SPI transmits data as a slave device.
+* Input          : transmitted data
+* Output         : None
+* Return         : None
+*******************************************************************************/
 void SPI_SlaveTransmit(uint8_t cData){
-  //Start transmission
+  /* Start transmission */
   SPDR = cData;
-  //Wait for transmission complete
+  /* Wait for transmission complete*/ 
   while(!(SPSR & (1<<SPIF)));
 }
 
-void modem_correction (uint8_t temp){
+/*******************************************************************************
+* Function Name  : DataCorrection_Transmit
+* Description    : Divide one byte data into two bytes by adding a header to each
+				   four bits of the data. the header will also serve as the address
+				   of each device. we can use the header for bit correction and for 
+				   ack protocols between master and slaves.
+* Input          : transmitted data
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void DataCorrection_Transmit (uint8_t temp){
   uint8_t temp1 = 0, temp2 = 0, i = 0;
-
-  //add header 0001 for bit correction
-  //shift correction protocol
-
-  //THIS HEADER WILL ALSO SERVE AS THE ADDRESS OF EACH DEVICE
-  //WE CAN USE THIS HEADER/ADDRESS FOR BIT CORRECTON AND FOR ACK PROTOCOLS BETWEEN MASTER AND SLAVES
-
-  //first configure MSB
-  temp1 = temp>>4;
-  temp1 |= 0x10;
-  //then configure LSB
-  temp2=temp&0x0f;
-  temp2|=0x10;
-
-  //Transmit each original byte 3 times 
-  //for failure correction
+  /* Configure the Most Siganificant Four bits of the Byte as: 0001 MSFB */
+  temp1 = (temp >> 4) | 0x10;
+  /* Configure the Least Siganificant Four bits of the Byte as: 0001 LSFB */
+  temp2 = (temp & 0x0f) | 0x10;
+  /* Transmit each original byte 3 times for failure correction*/
   for (i=0; i<3; i++) {
       SPI_SlaveTransmit(temp1);
       SPI_SlaveTransmit(temp2); 
   }  
+}
+
+/*******************************************************************************
+* Function Name  : DataCorrection_Receive
+* Description    : Divide one byte data into two bytes by adding a header to each
+				   four bits of the data. the header will also serve as the address
+				   of each device. we can use the header for bit correction and for 
+				   ack protocols between master and slaves.
+* Input          : transmitted data
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void DataCorrection_Receive (uint8_t temp){
+  uint8_t temp1 = 0, temp2 = 0, i = 0;
+  /* Configure the Most Siganificant Four bits of the Byte as: 0001 MSFB */
+  temp1 = (temp >> 4) | 0x10;
+  /* Configure the Least Siganificant Four bits of the Byte as: 0001 LSFB */
+  temp2 = (temp & 0x0f) | 0x10;
+  /* Transmit each original byte 3 times for failure correction*/
+  for (i=0; i<3; i++) {
+      SPI_SlaveTransmit(temp1);
+      SPI_SlaveTransmit(temp2); 
+  }  
+}
+
+/*******************************************************************************
+* Function Name  : Modem_CtrlRead
+* Description    : Request control register data from ST7540
+* Input          : None
+* Output         : None
+*******************************************************************************/
+void Modem_CtrlRead()
+{
+  /* Drive REG_DATA and RXTX high to request control register data from ST7540 */
+  digitalWrite(REG_DATA, HIGH);
+  digitalWrite(RXTX, HIGH);
+  byte temp1,temp2,temp3;
+  temp1 = SPI_SlaveReceive();
+  temp2 = SPI_SlaveReceive();
+  temp3 = SPI_SlaveReceive();
+  Serial.println(temp1,BIN);
+  Serial.println(temp2,BIN);
+  Serial.println(temp3,BIN);
 }
--- Milestone2_Transmit_Code/Milestone2_Transmit_Code.ino
+++ Milestone2_Transmit_Code/Milestone2_Transmit_Code.ino
@@ -1,69 +1,60 @@
-#define LED 2
-#define RXTX  15
+#define LED      2
+#define RXTX     15
 #define REG_DATA 18
-#define CD_PD 19
-
-// Comment this out for transmit code
-
-void SPI_SlaveInit(void);
-byte SPI_SlaveReceive(void);
-void modem_correction (uint8_t);
-
+#define CD_PD    19
+  /******************************************************************************
+     CD_PD   REG_DATA   RXTX            MODE                DIRECTION
+                HIGH    HIGH     Control regiter read   MOSI <-- RXD/OUTPUT
+                HIGH     LOW     Control regiter write  MISO --> TXD/INPUT
+                LOW     HIGH          Data read         MOSI <-- RXD/OUTPUT
+		        LOW      LOW          Data write        MISO --> TXD/INPUT
+   CD_PD High: Not transfer;  LOW: Transfer
+  *******************************************************************************/
 void setup() {
   pinMode(LED, OUTPUT);
-//  digitalWrite(LED, HIGH);
+  //digitalWrite(LED, HIGH);
   Serial.begin(115200);
   SPI_SlaveInit();
-  
-  //configure modem
-  pinMode(RXTX, OUTPUT);
-  pinMode(REG_DATA, OUTPUT);
-  pinMode(CD_PD, INPUT);
-  
   delay(100);
 }
  
 void loop() {
-
-  // The following commented code 1`reads from the control registers
-  /*digitalWrite(REG_DATA, HIGH);
-  digitalWrite(RXTX, HIGH);
-  byte temp1,temp2,temp3;
-  //while ((PINB  &   (1<<SCK))!=0);
-  temp1 = SPI_SlaveReceive();
-  temp2 = SPI_SlaveReceive();
-  temp3 = SPI_SlaveReceive();
-//delay(10);
-  Serial.println(temp1,BIN);
-  Serial.println(temp2,BIN);
-  Serial.println(temp3,BIN);*/  
-
-// STILL GOOD SEND CODE:
-  if (digitalRead(CD_PD)==HIGH){    // SEND A MESSAGE!
-    // The following code is where the good stuff happens
-    digitalWrite(REG_DATA, LOW);
-    digitalWrite(RXTX, LOW);
-    delay(10);
+  // Modem_CtrlRead()
+  if (digitalRead(CD_PD)==HIGH) // When the lines are not busy
+  {    
     Serial.println("Data transmission.");
-    digitalWrite(RXTX, HIGH);
-    modem_correction(0x15);
-    delay(100);
-    digitalWrite(RXTX, LOW);
+    DataCorrection_Transmit(0x15);
   }
-
 }
 
+/*******************************************************************************
+* Function Name  : SPI_SlaveInit
+* Description    : Configure Arduino pins for slave mode
+* Input          : None
+* Output         : None
+* Return         : SPDR
+*******************************************************************************/
 void SPI_SlaveInit(void){
-  pinMode(SCK,INPUT);
-  pinMode(MOSI,INPUT);
-  pinMode(SS,INPUT);
-  pinMode(MISO,OUTPUT);
-  //Enable SPI
+  /* Configure Arduino as a slave, Set MISO output, all other input */
+  pinMode(SCK,INPUT);   // PB5 --- GND
+  pinMode(SS,INPUT);    // PB2 --- ST7540_CLR/T
+  pinMode(MOSI,INPUT);  // PB3 --- ST7540_RXD
+  pinMode(MISO,OUTPUT); // PB4 --- ST7540_TXD
+  /* Configure pins for ST7540 */
+  pinMode(RXTX, OUTPUT);
+  pinMode(REG_DATA, OUTPUT);
+  pinMode(CD_PD, INPUT);
+  /* Enable SPI */
   SPCR = (1<<SPE)|(0<<MSTR);
-  
-  //SPSR =(1<<SPI2X);
 }
 
+/*******************************************************************************
+* Function Name  : SPI_SlaveReceive
+* Description    : Arduino SPI receives data from the buffer ( Modem )
+* Input          : None
+* Output         : None
+* Return         : SPDR
+*******************************************************************************/
 byte SPI_SlaveReceive(void){
   /* Wait for reception complete */
   while(!(SPSR & (1<<SPIF)));
@@ -71,33 +62,61 @@ byte SPI_SlaveReceive(void){
   return SPDR;
 }
 
+/*******************************************************************************
+* Function Name  : SPI_SlaveTransmit
+* Description    : Arduino SPI transmits data as a slave device.
+* Input          : transmitted data
+* Output         : None
+* Return         : None
+*******************************************************************************/
 void SPI_SlaveTransmit(uint8_t cData){
-  //Start transmission
+  /* Start transmission */
   SPDR = cData;
-  //Wait for transmission complete
+  /* Wait for transmission complete*/ 
   while(!(SPSR & (1<<SPIF)));
 }
 
-void modem_correction (uint8_t temp){
+/*******************************************************************************
+* Function Name  : DataCorrection_Transmit
+* Description    : Divide one byte data into two bytes by adding a header to each
+				   four bits of the data. the header will also serve as the address
+				   of each device. we can use the header for bit correction and for 
+				   ack protocols between master and slaves.
+* Input          : transmitted data
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void DataCorrection_Transmit (uint8_t temp){
   uint8_t temp1 = 0, temp2 = 0, i = 0;
-
-  //add header 0001 for bit correction
-  //shift correction protocol
-
-  //THIS HEADER WILL ALSO SERVE AS THE ADDRESS OF EACH DEVICE
-  //WE CAN USE THIS HEADER/ADDRESS FOR BIT CORRECTON AND FOR ACK PROTOCOLS BETWEEN MASTER AND SLAVES
-
-  //first configure MSB
-  temp1 = temp>>4;
-  temp1 |= 0x10;
-  //then configure LSB
-  temp2=temp&0x0f;
-  temp2|=0x10;
-
-  //Transmit each original byte 3 times 
-  //for failure correction
+  /* Configure the Most Siganificant Four bits of the Byte as: 0001 MSFB */
+  temp1 = (temp >> 4) | 0x10;
+  /* Configure the Least Siganificant Four bits of the Byte as: 0001 LSFB */
+  temp2 = (temp & 0x0f) | 0x10;
+  /* Transmit each original byte 3 times for failure correction*/
+  digitalWrite(REG_DATA, LOW);
+  digitalWrite(RXTX, LOW);
   for (i=0; i<3; i++) {
       SPI_SlaveTransmit(temp1);
       SPI_SlaveTransmit(temp2); 
   }  
+}
+
+/*******************************************************************************
+* Function Name  : Modem_CtrlRead
+* Description    : Request control register data from ST7540
+* Input          : None
+* Output         : None
+*******************************************************************************/
+void Modem_CtrlRead()
+{
+  /* Drive REG_DATA and RXTX high to request control register data from ST7540 */
+  digitalWrite(REG_DATA, HIGH);
+  digitalWrite(RXTX, HIGH);
+  byte temp1,temp2,temp3;
+  temp1 = SPI_SlaveReceive();
+  temp2 = SPI_SlaveReceive();
+  temp3 = SPI_SlaveReceive();
+  Serial.println(temp1,BIN);
+  Serial.println(temp2,BIN);
+  Serial.println(temp3,BIN);
 }
